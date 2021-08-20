15fd0d5599282e07e1df7d15782c8055a7de17d4
John Doe <john.doe@contoso.com>
2021-08-20 12:31:04 +0200

    First commit of Console Application

---
+++ Eggs/Eggs.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Eggs
+{
+    class Eggs
+    { 
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the ammount of eggs");
+            int numberOfEggs = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("The ammount of eggs are:" + numberOfEggs);
+
+            int dozen = numberOfEggs / 12;
+            int dozenLeft = numberOfEggs % 12;
+            Console.WriteLine(numberOfEggs + " eggs are equal to " + dozen + " dozen eggs and " + dozenLeft + " extra eggs left.");
+        }
+        
+    }
+}
